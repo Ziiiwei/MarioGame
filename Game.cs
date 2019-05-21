0efc995c325a5b472d7fc576dcfec84e0f25c82a
harrow.13 <harrow.13@osu.edu>
2019-05-21 11:38:15 -0400

    Changed background color.

--- Game.cs
+++ Game.cs
@@ -95,7 +95,7 @@ namespace Sprint0
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Azure);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
