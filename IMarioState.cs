a56dbdc8182f348eea549950aa529848619cb870
Keith Lou Jian Chin <chin.204@buckeyemail.osu.edu>
2019-05-25 15:18:28 -0400

    Added IMarioState

---
+++ IMarioState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2
+{
+    interface IMarioState
+    {
+        void FireMario();
+        void BigMario();
+        void SmallMario();
+    }
+}
