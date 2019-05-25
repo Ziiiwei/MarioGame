38f9590aa9e9b3a38e059ac349939a968dd0a454
Keith Lou Jian Chin <chin.204@buckeyemail.osu.edu>
2019-05-25 15:04:16 -0400

    Added IMario

---
+++ Mario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2.Interfaces
+{
+    public class Mario : IMario
+    {
+   
+        public void Draw()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Jump()
+        {
+
+        }
+        public void MovingRight()
+        {
+
+        }
+        public void MovingLeft()
+        {
+
+        }
+        public void FacingLeft()
+        {
+
+        }
+        public void FacingRight()
+        {
+
+        }
+
+        public void Crouch()
+        {
+
+        }
+    }
+}
