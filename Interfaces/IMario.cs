ef3c0fe6d29f6128482becc70ac07abcf0706616
Keith Lou Jian Chin <chin.204@buckeyemail.osu.edu>
2019-05-27 14:58:59 -0400

    Added item classes

--- Interfaces/IMario.cs
+++ Interfaces/IMario.cs
@@ -21,3 +21,7 @@ namespace Sprint2.Interfaces
 
     }
 }
+
+
+
+
---
+++ Sprites/RedMushroom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2.Sprites
+{
+    class RedMushroom : ISprite
+    {
+        private Texture2D Texture { get; set; }
+        private int Rows { get; set; }
+        private int Columns { get; set; }
+        private int currentFrame;
+        private int totalFrames;
+        private Vector2 location;
+        private int increment;
+
+        public RedMushroom(Texture2D texture, int rows, int columns, Vector2 theLocation)
+        {
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows * Columns;
+            location = theLocation;
+            increment = 1;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            int width = Texture.Width / Columns;
+            int height = Texture.Height / Rows;
+            int row = (int)((float)currentFrame / (float)Columns);
+            int column = currentFrame % Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+
+        }
+    }
+}
