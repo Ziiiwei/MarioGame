8e39a959a0dd26deda08a15033c9a722e87b738a
Ziwei Jin <jin.517@osu.edu>
2019-06-04 16:41:55 -0400

    Physics Interface

---
+++ MarioGame/Interfaces/IPhysics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Gamespace
+{
+    public interface IPhysics
+    {
+        Vector2 position { get; set; }
+        Vector2 velocity { get; set; }
+
+        int acceleration { get; set; }
+
+        void MoveLeft();
+        void MoveRight();
+        void FreeFall();
+        void Jump();
+        void SpeedUp();
+        void SlowDown();
+        void Stop();
+    }
+}
