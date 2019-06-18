992085f8a098ab28d14d414942e06c6f22545711
Keith Lou Jian Chin <chin.204@buckeyemail.osu.edu>
2019-06-18 11:39:03 -0400

    Made CSV to JSON converter. Still working on it.

---
+++ MarioGame/Data/CSVtoJSON.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.ApplicationServices;
+
+namespace Gamespace
+{
+    public class CSVtoJSON
+    {
+        string[] lines = System.IO.File.ReadAllLines(@"world.csv");
+
+
+    }
+}
