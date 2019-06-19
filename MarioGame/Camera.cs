5c3052b583be0e79adfdb78e0dbd590b09826e0b
Rayan Hamza <rayanhamza23@outlook.com>
2019-06-19 14:04:26 -0400

    Camera should not move left unless you do the wrap around. Otherwise, it moves when Mario is at the center of the X.

--- MarioGame/Camera.cs
+++ MarioGame/Camera.cs
@@ -12,22 +12,32 @@ namespace Gamespace
     internal class Camera
     {
         public Matrix Transform { get; private set; }
+        private float offset;
 
         public Camera(Point location)
         {
             Transform = Matrix.CreateTranslation(new Vector3(-location.X, -location.Y, 0));
+            offset = 0;
         }
         public Camera() : this(Point.Zero) { }
         public void LookAt(Point location)
         {
             Transform = Matrix.CreateTranslation(new Vector3(-location.X, -location.Y, 0));
+            offset = 0;
         }
 
         public void Update()
         {
-            Vector2 center = new Vector2(World.Instance.Mario.GetCenter().X - MarioGame.ScreenWidth / 4, 
+            if(World.Instance.Mario.PositionOnScreen.X >= Transform.Left.X + MarioGame.ScreenWidth / 4)
+                MoveRight();
+        }
+
+        public void MoveRight()
+        {
+            Vector2 center = new Vector2(World.Instance.Mario.GetCenter().X - MarioGame.ScreenWidth / 4,
                 World.Instance.Mario.GetCenter().Y - MarioGame.ScreenHeight / 4);
-            Transform = Matrix.CreateTranslation(-center.X, -center.Y, 0);
+            Transform = Matrix.CreateTranslation(-center.X, 0/*-center.Y*/, 0);
+            offset++;
         }
         
 
