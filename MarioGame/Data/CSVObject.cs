f15860593fcfb938983f493bec16fb1917852ac0
Keith Lou Jian Chin <chin.204@buckeyemail.osu.edu>
2019-06-19 18:55:02 -0400

    Parser now parses JSON File with Header!! Hallelujah!!!

--- MarioGame/Data/CSVObject.cs
+++ MarioGame/Data/CSVObject.cs
@@ -13,15 +13,15 @@ namespace Gamespace
         public String Name { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
+
+        public String State { get; set; }
    
-        public CSVObject(String name, int x, int y)
+        public CSVObject(String name, int x, int y, String state)
         {
             Name = name;
             X = x;
             Y = y;
+            State = state;
         }
-    
-
-    
     }
 }
--- MarioGame/Data/CSVtoJSON.cs
+++ MarioGame/Data/CSVtoJSON.cs
@@ -18,61 +18,56 @@ namespace Gamespace
 
         }
 
-        public static void convert()
+        public static void ConvertCSVtoJSON()
         {
             StreamReader sr = new StreamReader("MarioGame/Data/level1example.csv");
 
             String [][] data = new String[15][];
             data = File.ReadLines("MarioGame/Data/level1example.csv").Select(x => x.Split(',')).ToArray();
-
-
-           
             int _X = 0;
             int _Y = 0;
             int row = 0;
             var JsonList = new List<CSVObject>();
             String name = "";
-
+            String state = null;
             String path = "MarioGame/Data/level1objects.json";
-
-            using (Stream stream = File.Open(path, FileMode.Create))
+            for (int i = 0; i < data.GetLength(0); i++)
             {
-                
-                for (int i = 0; i < data.GetLength(0); i++)
+                for (int j = 0; j < 10; j++)
                 {
-                    for (int j = 0; j < 10; j++)
+
+                    // The first column is the row number
+                    if (j == 0)
                     {
-
-                        // The first column is the row number
-                        if (j == 0)
-                        {
-                            row = Int32.Parse(data[i][j]);
-                        }
-
-                        /* If the array location is not empty, grab the name
-                        *  calculat the X and Y coordinates, and serialize as 
-                        *  a JSON object.
-                        */
-                        else if (!(data[i][j] == ""))
-                        {
-                            name = data[i][j];
-                            _X = (i * 32);
-                            _Y = (j * 32);
-                            CSVObject obj = new CSVObject(name, _X, _Y);
-                            string output = JsonConvert.SerializeObject(obj);
-                            JsonList.Add(obj);
-
-                        }
-
+                        row = Int32.Parse(data[i][j]);
+                    }
+                    /* If the array location is not empty, grab the name
+                     *  calculat the X and Y coordinates, and serialize as 
+                     *  a JSON object.
+                     */
+                    else if (!(data[i][j] == ""))
+                    {
+                        name = data[i][j];
+                        _X = (i * 32);
+                        _Y = (j * 32);
+                        state = null;
+                        CSVObject obj = new CSVObject(name, _X, _Y, state);
+                        string output = JsonConvert.SerializeObject(obj);
+                        JsonList.Add(obj);
                     }
                 }
             }
-            using (StreamWriter file = File.CreateText(@"MarioGame/Data/level1objects.json"))
+            var JsonListWithHeader = new
+            {
+
+                Entities = JsonList
+
+            };
+            using (StreamWriter file = File.CreateText(@path))
             {
                 JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, JsonList);
+                serializer.Serialize(file, JsonListWithHeader);
             }
-
         }
     }
 }
