3fc835b2d7199a9e4e9be2996c5381e3eaaf72e9
Ziwei Jin <jin.517@osu.edu>
2019-06-19 17:34:38 -0400

    Camera fixed

--- MarioGame/Camera.cs
+++ MarioGame/Camera.cs
@@ -15,19 +15,23 @@ namespace Gamespace
 
         public Camera(Point location)
         {
-            Transform = Matrix.CreateTranslation(new Vector3(-location.X, -location.Y, 0));
+            Transform = Matrix.CreateTranslation(new Vector3(-location.X, 0, 0));
         }
         public Camera() : this(Point.Zero) { }
         public void LookAt(Point location)
         {
-            Transform = Matrix.CreateTranslation(new Vector3(-location.X, -location.Y, 0));
+            Transform = Matrix.CreateTranslation(new Vector3(-location.X, 0, 0));
         }
 
         public void Update()
         {
-            Vector2 center = new Vector2(World.Instance.Mario.GetCenter().X - MarioGame.ScreenWidth / 4, 
-                World.Instance.Mario.GetCenter().Y - MarioGame.ScreenHeight / 4);
-            Transform = Matrix.CreateTranslation(-center.X, -center.Y, 0);
+            float _dX = World.Instance.Mario.GetCenter().X - MarioGame.ScreenWidth / 4;
+            float current_X = World.Instance.Mario.GetCenter().X + Transform.M14;
+            if (_dX > -Transform.M14 && current_X > MarioGame.ScreenWidth / 4)
+            {
+                Transform = Matrix.CreateTranslation(-_dX, 0, 0);
+            }
+            
         }
         
 
