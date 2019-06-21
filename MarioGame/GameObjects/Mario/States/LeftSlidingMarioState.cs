66fc0d55657540293deb5108bb97f1b60f1b6c2c
Rayan Hamza <rayanhamza23@outlook.com>
2019-06-21 15:14:16 -0400

    Added Sliding States

---
+++ MarioGame/GameObjects/Mario/States/LeftSlidingMarioState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gamespace;
+
+namespace Gamespace.States
+{
+    class LeftSlidingMarioState : MovingMarioState
+    {
+        private IMario mario;
+
+        public LeftSlidingMarioState(IMario mario) : base(mario)
+        {
+            this.mario = mario; 
+        }
+
+        public override void Crouch()
+        {
+            mario.State = new RightCrouchingMarioState(mario);
+            mario.UpdateArt();
+        }
+
+        public override void Jump()
+        { 
+            mario.State = new RightJumpingMarioState(mario);
+            mario.UpdateArt();
+        }
+
+
+        public override void MoveLeft()
+        {
+            mario.State = new LeftStandingMarioState(mario);
+            mario.UpdateArt();
+        }
+
+        public override void MoveRight()
+        {
+            mario.State = new RightWalkingMarioState(mario);
+            mario.UpdateArt();
+        }
+    }
+}
---
+++ MarioGame/GameObjects/Mario/States/RightSlidingMarioState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gamespace;
+using Gamespace.Interfaces;
+
+namespace Gamespace.States
+{
+    class RightSlidingMarioState : MovingMarioState
+    {
+        private IMario mario;
+
+        public RightSlidingMarioState(IMario mario) : base(mario)
+        {
+            this.mario = mario;
+        }
+
+        public override void Crouch()
+        {
+            mario.State = new LeftCrouchingMarioState(mario);
+            mario.UpdateArt();
+        }
+
+        public override void Jump()
+        {
+            mario.State = new LeftJumpingMarioState(mario);
+            mario.UpdateArt();
+        }
+
+        public override void MoveLeft()
+        {
+            mario.State = new LeftWalkingMarioState(mario);
+            mario.UpdateArt();
+        }
+
+        public override void MoveRight()
+        {
+            mario.State = new RightStandingMarioState(mario);
+            mario.UpdateArt();
+        }
+    }
+}
