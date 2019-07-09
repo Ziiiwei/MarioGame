42baa7d942765d2d1b384d16675a0d09e6f33ddf
Ziwei Jin <jin.517@osu.edu>
2019-07-09 06:58:17 -0400

    Starting on Interface

---
+++ MarioGame/Animation/IKeyFrame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Gamespace.Animaiton
+{
+    interface IKeyFrame
+    {
+        IKeyFrame Previous { get; set; }
+        IKeyFrame Next { get; set; }
+        Vector2 FramePoint { get; set; }
+
+     
+        void ToNext();
+    }
+}
