7eb38553431cc2b1bfa119fceecf667bbd0e5302
Ziwei Jin <jin.517@osu.edu>
2019-07-10 12:11:11 -0400

    Push Before class Still need to change world to make it support aniamtion

--- MarioGame/Animation/AnimationFactory.cs
+++ MarioGame/Animation/AnimationFactory.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gamespace.Commands;
+using Microsoft.Xna.Framework;
 
 namespace Gamespace.Animation
 {
     public class AnimationFactory
     {
+        private List<(Side,int,Type)> testMarioAnimation;
         static AnimationFactory() { }
         private AnimationFactory()
         {
-
+            testMarioAnimation = new List<(Side side, int distance, Type command)>
+            {
+                (Side.Right, 100, typeof(MarioMoveRightCommand)),
+                (Side.Up, 50, typeof(MarioJumpCommand)),
+                (Side.Left,200,typeof(MarioMoveLeftCommand)),
+                (Side.None,20,typeof(MarioCrouchCommand)),
+                (Side.Up, 50, typeof(MarioJumpCommand))
+            };
 
         }
 
