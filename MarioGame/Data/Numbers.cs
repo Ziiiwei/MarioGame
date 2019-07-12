66c1b8d8770b762392c45b1b5004fce0b6fde9af
harrow.13 <harrow.13@osu.edu>
2019-07-12 17:01:21 -0400

    Actual fix for collision detection.

--- MarioGame/Data/Numbers.cs
+++ MarioGame/Data/Numbers.cs
@@ -33,5 +33,6 @@ namespace Gamespace.Data
         public const float PROJECTILE_LEFT_OFFSET = -18;
         public const int CAMERA_START_X= 0;
         public const int FOREGROUND_DRAW_PRIORITY = 1;
+        public const int LEVEL1_BLOCK_WIDTH = 206;
     }
 }
