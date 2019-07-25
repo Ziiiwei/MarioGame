09711a944e2bf3cf8f9bea707e0642524acd3d3c
harrow.13 <harrow.13@osu.edu>
2019-07-24 20:12:31 -0400

    Select character done.

---
+++ MarioGame/PlayerCharactors/Soldier/Soldier.cs
@@ -0,0 +1,19 @@
+﻿using Gamespace;
+using Gamespace.Multiplayer;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamespace
+{
+    class Soldier : Character
+    {
+        public Soldier(Vector2 positionOnScreen, Scoreboard scoreboard) : base(positionOnScreen, scoreboard)
+        {
+
+        }
+    }
+}
